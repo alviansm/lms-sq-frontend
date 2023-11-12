cbc9489ef514112a9c405d2c668d198208866d46
alviansm <sandalproject7@gmail.com>
2023-11-12 21:49:28 +0700

    fix page/example

--- app/quiz/example/page.tsx
+++ app/quiz/example/page.tsx
@@ -1,17 +1,6 @@
 import React from "react";
-import { useRouter } from "next/router";
 
 const QuizExample = () => {
-    const router = useRouter();
-    const query = router.query;
-    const waktu = query['waktu'];
-    const jeda = query['jeda'];
-    const jumlah_soal = query['jumlah_soal'];
-    const tingkat_kesulitan = query['tingkat_kesulitan'];
-    const deskripsi = query['deskripsi'];
-    const judul = query['judul'];
-    const organizer_image_url = query['organizer_image_url'];
-    const organizer = query['organizer'];
     
     return (
         <div>
